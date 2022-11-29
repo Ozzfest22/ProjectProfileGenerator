e335058d9b047edfe3fd0664e58d3b3dbdca84f8
Ozzfest22 <glossiom@gmail.com>
2022-11-29 02:35:36 -0500

    Actualizacion final de proyecto

--- ProjectProfileGenerator/Controllers/GeneratorController.py
+++ ProjectProfileGenerator/Controllers/GeneratorController.py
@@ -24,64 +24,63 @@ def obtener_perfil(Id):
     conexion.close()
     return perfil
 
-# Declaramos la deteccion de rostros
+
 detros = mp.solutions.face_detection
 rostros = detros.FaceDetection(min_detection_confidence= 0.8, model_selection=0)
-# Dibujo
+
 dibujorostro = mp.solutions.drawing_utils
 
-# Realizamos VideoCaptura
+
 cap = cv2.VideoCapture(0)
-# Make the WSGI interface available at the top level so wfastcgi can get it.
+
 
 def generate():
      numero = 0
 
      while True:
-    # Leemos los fotogramas
         ret, frame = cap.read()
-        # Leemos imagen
+        
         img =cv2.imread("ProjectProfileGenerator/static/img.png")
 
         img = cv2.resize(img, (0, 0), None, 0.18, 0.18)
         ani, ali, c = img.shape
 
-        # Correccion de color
+        
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         
-        # Procesamos
+        
         resrostros = rostros.process(rgb)
 
-        # Deteccion
+        
         if resrostros.detections is not None:
-            # Registramos
+            
             for rostro in resrostros.detections:
-                # Extraemos informacion de ubicacion
+                
                 al, an, c = frame.shape
                 box = rostro.location_data.relative_bounding_box
                 xi, yi, w, h = int(box.xmin * an), int(box.ymin * al), int(box.width * an), int(box.height * al)
                 xf, yf = xi + w, yi + h
 
-                # Dibujamos
+                
                 cv2.rectangle(frame, (xi, yi), (xf, yf), (255, 255, 0), 1)
                 frame[10:ani + 10, 10:ali+10] = img
 
-                # Informacion
+                
                 info = DeepFace.analyze(rgb, actions=['age', 'gender', 'race', 'emotion'], enforce_detection= False)
 
-                # Edad
+                
                 edad = info['age']
 
-                # Emociones
+                
                 emociones = info['dominant_emotion']
 
-                # Race
+                
                 race = info['dominant_race']
 
-                # Genero
+                
                 gen = info['gender']
-                #print(str(gen) + " de " + str(edad) + " años de edad, con estado de animo " + str(emociones) + " de etnia " + str(race))
+                
 
                 # Traducimos
                 if gen == 'Man':
@@ -150,7 +149,7 @@ def generate():
                     if race == 'latino hispanic':
                         race = 'latina'
 
-                # Mostramos info
+                
                 cv2.putText(frame, str(gen), (65, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
                 cv2.putText(frame, str(edad), (75, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
                 cv2.putText(frame, str(emociones), (75, 135), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
@@ -166,8 +165,7 @@ def generate():
                 numero += 1         
 
 
-        # Mostramos los fotogramas
-        #cv2.imshow(" Deteccion de Edad ", frame)
+        
         suc, encode = cv2.imencode('.jpg', frame)
         frame = encode.tobytes()
         
